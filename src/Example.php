4d2667ea9ac0dafefdccc3934b0cbebe9d1acfad
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-03-20 00:43:33 +0100

    reproduce

--- src/Example.php
+++ src/Example.php
@@ -2,14 +2,28 @@
 
 namespace PhpCsFixer\ReproducePlayground;
 
+use App\Entity\Processus\Champ\Champ;
+use App\Entity\Processus\Processus;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-/**
- * @internal
- */
-class Example
+class InformationsEtapeDto
 {
-
-
-
-    function foo() {}
+    public function __construct(
+        /** Nécessaire pour le formulaire */
+        public Processus $processus,
+        #[NotBlank]
+        #[Length(max: 100)]
+        public string $libelle {
+            set(?string $value) {
+                $this->libelle = $value ?? "";
+            }
+        },
+        /**
+         * @var iterable<Champ<mixed>>
+         */
+        public iterable $champsAffiches = [],
+        public bool $archivable = false,
+        public bool $commentable = false,
+    ) {}
 }
